c6c1c4cca5ac8dc3e0c9244f133a433c547717a0
Bryan Barcelona <131413239+bryanbarcelona@users.noreply.github.com>
2026-03-10 05:27:26 +0100

    chore(pixelporter): remove deprecated implementation

--- src/brybox/core/pixelporter/__init__.py
+++
@@ -1,6 +0,0 @@
-"""PixelPorter - Photo ingestion and processing orchestrator."""
-
-from .orchestrator import PushResult, push_photos
-from .protocols import Deduplicator, FileProcessor, ProcessResult
-
-__all__ = ['Deduplicator', 'FileProcessor', 'ProcessResult', 'PushResult', 'push_photos']
